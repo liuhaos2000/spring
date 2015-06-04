3b04102f0d6d6d28fc280f5f26738b284a7ada80
liuhaos2000 <liuhaos2000@163.com>
2015-06-04 17:25:04 +0800

    all update

---
+++ src/main/java/lh/abs/dao/MUserMapper.java
@@ -0,0 +1,17 @@
+package lh.abs.dao;
+
+import lh.abs.model.MUser;
+
+public interface MUserMapper {
+    int deleteByPrimaryKey(String userId);
+
+    int insert(MUser record);
+
+    int insertSelective(MUser record);
+
+    MUser selectByPrimaryKey(String userId);
+
+    int updateByPrimaryKeySelective(MUser record);
+
+    int updateByPrimaryKey(MUser record);
+}
---
+++ src/main/java/lh/abs/model/MUser.java
@@ -0,0 +1,45 @@
+package lh.abs.model;
+
+import java.util.Date;
+
+public class MUser {
+    private String userId;
+
+    private String userName;
+
+    private String userPassword;
+
+    private Date birthday;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId == null ? null : userId.trim();
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName == null ? null : userName.trim();
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword == null ? null : userPassword.trim();
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+}
